51699da69c345214f6d48e4dc936a020f1309363
SnrDave <111582337+SnrDave@users.noreply.github.com>
2023-06-07 14:50:29 +0300

    Update Footer.js
    footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>SnrDave • © 2023</p>
+          <p>David©2023</p>
         </Flex>
       </footer>
     </Box>
